49160ccf6ac7868977d1a3d56e8550d199fed4dc
Michael Lustenberger <mic@inofix.ch>
2019-05-17 15:19:19 +0200

    Photoresistors
    Add a script to simulate light situations for my sensors..

---
+++ simulations/photoresistorsim.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+# This script simply generates a table of the voltage to expect
+# if I combine my photoresistors with some of my resitors from stock..
+
+# As there is no strong sunlight at the moment here, I will have to
+# postpone the real life testing..
+
+# Setup:
+# --[resistor]-+--(~)---- 5V
+# |            |
+# = 0V        {?}
+#              |
+#              = 0V
+
+voltage=5.
+
+resistor=[59,180,220,453,750,1000,10000,20000]
+measurement=[2000000, 1000000, 600000, 200000, 100000, 80000, 40000, 20000, 10000, 8000, 6000, 4000, 2000, 1000, 800, 600, 400, 200, 100, 80, 60, 40, 20, 8, 2]
+
+def measure(r,p):
+    return round(voltage/(r+p)*r,4)
+
+s="\t"
+for r in resistor:
+    s+=str(r)+"\t"
+print s
+
+for m in measurement:
+    s=str(m) + "\t"
+    for r in resistor:
+        s+=str(measure(m,r)) + "\t"
+    print s
+
+
+
+ 
