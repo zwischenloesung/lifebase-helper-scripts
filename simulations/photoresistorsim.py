f674562457bd7b53d06b365cf3b9c238b6f0a047
Michael Lustenberger <mic@inofix.ch>
2019-06-01 12:53:13 +0200

    User Feedback
    On a users request: add some explations in the output.

--- simulations/photoresistorsim.py
+++ simulations/photoresistorsim.py
@@ -6,13 +6,6 @@
 # As there is no strong sunlight at the moment here, I will have to
 # postpone the real life testing..
 
-# Setup:
-# --[resistor]-+--(~)---- 5V
-# |            |
-# = 0V        {?}
-#              |
-#              = 0V
-
 voltage=5.
 
 resistor=[59,180,220,453,750,1000,10000,20000]
@@ -21,7 +14,19 @@ measurement=[2000000, 1000000, 600000, 200000, 100000, 80000, 40000, 20000, 1000
 def measure(r,p):
     return round(voltage/(r+p)*r,4)
 
-s="\t"
+print ""
+print "Table of expected voltage for a given resistor and light exposure"
+print "-----------------------------------------------------------------"
+print ""
+print "Setup:"
+print " -----[R]---+--(~)---- 5V"
+print " |          |"
+print " = 0V      {?}"
+print "            |"
+print "            = 0V"
+print ""
+
+s="~ \\ R\t"
 for r in resistor:
     s+=str(r)+"\t"
 print s
